55b4f433831d4cb01a44947ddc19d7584778be6e
haixun00 <haixun00@gmail.com>
2017-05-30 10:53:01 +0800

    object_tracker_ros: v1.3: finished coding

--- src/test.cpp
+++ src/test.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm> 
+#include <random>
 
 #include <neural_cam_ros/obstacle.h>
 #include <neural_cam_ros/obstacleStack.h>
@@ -25,9 +26,23 @@
 
 using namespace std;
 using namespace dlib;
+using namespace cv;
+
+// <--------- kalman filter parameters --------->
+int stateSize = 6;
+int measSize = 4;
+int contrSize = 0;
+unsigned int type = CV_32F;
+double ticks = 0;
+
+//random number generator
+//srand (time(NULL));
+std::random_device rd;
+std::mt19937 mt(rd()); 							 	// seed the Mersenne Twister generator
+std::uniform_int_distribution<> dist(1000, 9999); 	// define the range
 
 // callback controller
-bool setflag = true;
+bool setFlag = true;
 int noObjectsCount = 0;
 
 // datatype
@@ -37,11 +52,76 @@ typedef struct {
     cv::Point2f topLeft;
     cv::Point2f bottomRight;
 
+    KalmanFilter kalmanTracker;
+    Mat objectState;
+    Mat objectMeas;
+
 } object;
 
 // container
 std::vector <object> prev_objects;
 std::vector <object> curr_objects;
+
+
+int generateRandomID(){
+	return dist(mt);
+}
+
+
+KalmanFilter create_kalmanTracker(){
+
+	KalmanFilter kf;
+
+	// initialize kalman parameters	
+	kf.init(stateSize, measSize, contrSize, type);
+
+	//Mat procNoise(stateSize, 1, type)
+	// [E_x,E_y,E_v_x,E_v_y,E_w,E_h]
+
+	// Transition State Matrix A
+	// Note: set dT at each processing step!
+	// [ 1 0 dT 0  0 0 ]
+	// [ 0 1 0  dT 0 0 ]
+	// [ 0 0 1  0  0 0 ]
+	// [ 0 0 0  1  0 0 ]
+	// [ 0 0 0  0  1 0 ]
+	// [ 0 0 0  0  0 1 ]
+
+	setIdentity(kf.transitionMatrix);
+
+	// Measure Matrix H
+	// [ 1 0 0 0 0 0 ]
+	// [ 0 1 0 0 0 0 ]
+	// [ 0 0 0 0 1 0 ]
+	// [ 0 0 0 0 0 1 ]
+
+	kf.measurementMatrix = cv::Mat::zeros(measSize, stateSize, type);
+	kf.measurementMatrix.at<float>(0) = 1.0f;
+	kf.measurementMatrix.at<float>(7) = 1.0f;
+	kf.measurementMatrix.at<float>(16) = 1.0f;
+	kf.measurementMatrix.at<float>(23) = 1.0f;
+
+	// Process Noise Covariance Matrix Q
+	// [ Ex 0  0    0 0    0 ]
+	// [ 0  Ey 0    0 0    0 ]
+	// [ 0  0  Ev_x 0 0    0 ]
+	// [ 0  0  0    1 Ev_y 0 ]
+	// [ 0  0  0    0 1    Ew ]
+	// [ 0  0  0    0 0    Eh ]
+
+	//cv::setIdentity(kf.processNoiseCov, cv::Scalar(1e-2));
+	kf.processNoiseCov.at<float>(0) = 1e-2;
+	kf.processNoiseCov.at<float>(7) = 1e-2;
+	kf.processNoiseCov.at<float>(14) = 2.0f;
+	kf.processNoiseCov.at<float>(21) = 1.0f;
+	kf.processNoiseCov.at<float>(28) = 1e-2;
+	kf.processNoiseCov.at<float>(35) = 1e-2;
+
+	// Measures Noise Covariance Matrix R
+	setIdentity(kf.measurementNoiseCov, cv::Scalar(1e-1));
+
+	return kf;
+}
 
 
 float calculate_iou(cv::Point2f tl_a, cv::Point2f tl_b, cv::Point2f br_a, cv::Point2f br_b){
@@ -71,8 +151,11 @@ float calculate_iou(cv::Point2f tl_a, cv::Point2f tl_b, cv::Point2f br_a, cv::Po
 
 
 void subCallback(const neural_cam_ros::obstacleStack::ConstPtr& msg)
-{
-   //ROS_INFO("Hearing From: [%s]", msg->stack_name.c_str());
+{ 
+   // time keeping
+   double precTick = ticks;
+   ticks = (double) cv::getTickCount();
+   double dT = (ticks - precTick) / cv::getTickFrequency();  //seconds
 
    int num_objects_det_curr = msg->stack_len;
    int num_objects_det_prev = (int) prev_objects.size();
@@ -86,12 +169,20 @@ void subCallback(const neural_cam_ros::obstacleStack::ConstPtr& msg)
 
 	   			object tempStorage;
 
-	   			tempStorage.id = i+1;	//generate ID
+	   			tempStorage.id = generateRandomID();	//generate ID
 	   			tempStorage.notFoundCounter = 0;
 	   			tempStorage.topLeft.x = (float) msg->stack_obstacles[i].topleft.x;
 	   			tempStorage.topLeft.y = (float) msg->stack_obstacles[i].topleft.y;
 	   			tempStorage.bottomRight.x = (float) msg->stack_obstacles[i].bottomright.x;
 	   			tempStorage.bottomRight.y = (float) msg->stack_obstacles[i].bottomright.y;
+
+	   			cv::Mat state_set(stateSize, 1, type);  // [x,y,v_x,v_y,w,h]
+				cv::Mat meas_set(measSize, 1, type);    // [z_x,z_y,z_w,z_h]
+
+				tempStorage.objectState = state_set;
+				tempStorage.objectMeas = meas_set;
+
+				tempStorage.kalmanTracker = create_kalmanTracker();
 
 	   			prev_objects.push_back(tempStorage);
    			}
@@ -99,12 +190,13 @@ void subCallback(const neural_cam_ros::obstacleStack::ConstPtr& msg)
    			setFlag = false;
    		}
 
-   }else{		// subsequent frame
+   }else{		// subsequent frame tracking
 
    		/* -----> store objects into the curr_objects -----> */
-   		object tempStorage;
 
    		for(int i = 0; i < num_objects_det_curr; i++){
+
+   			object tempStorage;
 
    			tempStorage.topLeft.x = (float) msg->stack_obstacles[i].topleft.x;
    			tempStorage.topLeft.y = (float) msg->stack_obstacles[i].topleft.y;
@@ -115,25 +207,24 @@ void subCallback(const neural_cam_ros::obstacleStack::ConstPtr& msg)
    		}
    		/* <----- store objects into the curr_objects <----- */
 
-
    		bool situation = true;
    		int max_mat_dim = max(num_objects_det_curr, num_objects_det_prev);
 
    		matrix<int> cost(max_mat_dim,max_mat_dim);
 
-   		if(num_objects_det_curr <= num_objects_det_prev){       // Case 1: When detected objects in the current window less or equal to the previous detected
+   		if(num_objects_det_curr <= num_objects_det_prev){       	// Case 1: When detected objects in the current window less or equal to the previous detected
 
    			cout << "Case 1: curr <= prev" << endl;
 
 	   		for(int i = 0; i < num_objects_det_prev; i++){
 
-	   			for(int j = 0; j < num_objects_det_prev; j++){	//check which give the highest IOU
+	   			for(int j = 0; j < num_objects_det_prev; j++){		//check which give the highest IOU
 
 	   				if(j >= num_objects_det_curr){
-	   					
+
 	   					// missing detections in the current frame
 
-	   					cost(i,j) = -1;							//assign dummy value
+	   					cost(i,j) = -1;								//assign dummy value
 
 	   				}else{
 
@@ -143,7 +234,7 @@ void subCallback(const neural_cam_ros::obstacleStack::ConstPtr& msg)
 
 	   					cost(i,j) = (int) (iou_value*1000);
 
-	   					//cout << "IOU: " << iou_value << endl;
+	   					
 
 	   				}
 	   			}
@@ -161,8 +252,6 @@ void subCallback(const neural_cam_ros::obstacleStack::ConstPtr& msg)
 
 	   				if(i >= num_objects_det_prev){
 
-	   					//cout << "Possible New Detections: " << (num_objects_det_curr - num_objects_det_prev) << endl;
-
 	   					cost(i,j) = -1;				//assign dummy value
 
 	   				}else{
@@ -171,74 +260,126 @@ void subCallback(const neural_cam_ros::obstacleStack::ConstPtr& msg)
 
 		   				cost(i,j) = (int) (iou_value*1000);
 
-		   				//cout << "IOU: " << iou_value << endl;
 		   			}
-
 	   			}
-
 	   		}
-
 	   	}
 
-	   	// This prints optimal assignments:  [2, 0, 1] which indicates that we should assign
-    	// the person from the first row of the cost matrix to job 2, the middle row person to
-    	// job 0, and the bottom row person to job 1.
+	   //	cout << "cost: " << endl;
+	   //	cout << cost << endl;
 
-	   	std::vector<long> assignment = dlib::max_cost_assignment(cost);
-    	for (unsigned int i = 0; i < assignment.size(); i++){
+	   	// // example: max_cost_assignment_ex.cpp
+	   	// // This prints optimal assignments:  [2, 0, 1] which indicates that we should assign
+    	// // the person from the first row of the cost matrix to job 2, the middle row person to
+    	// // job 0, and the bottom row person to job 1.	
+	   	// std::vector<long> assignment = dlib::max_cost_assignment(cost);			//do Hungarian assignment
 
-        	cout << "Assignment: " << (int) assignment[i] << std::endl;
+    	// for (unsigned int i = 0; i < assignment.size(); i++){
 
-        	int assignment_to = (int) assignment[i];
+     //    	cout << "Assignment: " << (int) assignment[i] << std::endl;
 
-        	if(situation){
+     //    	int assignment_to = (int) assignment[i];
 
-        		// if current detected objects less than the previous detected ones, assign the current detected ones with previous id
-        		// mark the rest as undetected for this current iteration
+     //    	if(situation){
 
-        		if(assignment_to >= num_objects_det_curr) {		// assigned to hoax object
+     //    		// if current detected objects less than the previous detected ones, assign the current detected ones with previous id
+     //    		// mark the rest as undetected for this current iteration
 
-        			// mark as undetected for this round
+     //    		if(assignment_to >= num_objects_det_curr) {		// assigned to hoax object
 
-        			// TODO:
+     //    			// mark as UNDETECTED OBJECTS for this round
 
-        			if(prev_objects[i].notFoundCounter < 5){ 	//assess if the previous item need to be included into the current object list or not
+     //    			if(prev_objects[i].notFoundCounter < 5){ 	//assess if the previous item need to be included into the current object list or not
 
-        				//kalman assignment here:
+     //    				// kalman estimate here:
 
-        				prev_objects[i].notFoundCounter++;
-        				curr_objects.push_back(prev_objects[i]);
-        			}
+     //    				prev_objects[i].kalmanTracker.statePost = prev_objects[i].objectState;	//estimate
 
-        		}else{
+     //    				prev_objects[i].notFoundCounter++;
+     //    				curr_objects.push_back(prev_objects[i]);
+     //    			}
 
-        			curr_objects[assignment_to].id = prev_objects[i].id;
-        		}
+     //    		}else{		
 
-        	}else{
+     //    			//take current measurement
+     //    			curr_objects[assignment_to].objectMeas.at<float>(0) = curr_objects[assignment_to].topLeft.x + (curr_objects[assignment_to].bottomRight.x - curr_objects[assignment_to].topLeft.x)/2;;
+					// curr_objects[assignment_to].objectMeas.at<float>(1) = curr_objects[assignment_to].topLeft.y + (curr_objects[assignment_to].bottomRight.y - curr_objects[assignment_to].topLeft.y)/2;
+					// curr_objects[assignment_to].objectMeas.at<float>(2) = curr_objects[assignment_to].bottomRight.x - curr_objects[assignment_to].topLeft.x;
+					// curr_objects[assignment_to].objectMeas.at<float>(3) = curr_objects[assignment_to].bottomRight.y - curr_objects[assignment_to].topLeft.y;
 
-        	 	if(i >= num_objects_det_prev){
+					// curr_objects[assignment_to].kalmanTracker = prev_objects[i].kalmanTracker;
+					// curr_objects[assignment_to].kalmanTracker.correct(curr_objects[assignment_to].objectMeas);		//correction
+     //    			curr_objects[assignment_to].id = prev_objects[i].id;
+     //    		}
 
-        	 		//mark as new objects detected (generate new id/kalman filter)
+     //    	}else{
 
-        	 		// TODO:
+     //    	 	if(i >= num_objects_det_prev){
 
-        	 		curr_objects[assignment_to].id = 19992;
-        	 		curr_objects[assignment_to].notFoundCounter = 0;
+     //    	 		// mark as NEW OBJECTS detected (generate new id/kalman filter)
 
-        	 		//insert new kalman filter here
+     //    	 		curr_objects[assignment_to].id = generateRandomID();
+     //    	 		curr_objects[assignment_to].notFoundCounter = 0;
 
-        	 	}else{
+     //    	 		// skip measurement assignment as we did it before 
 
-        	 		curr_objects[assignment_to].id = prev_objects[i].id;
-        	 	}
+	   	// 			cv::Mat state_set(stateSize, 1, type);  // [x,y,v_x,v_y,w,h]
+					// cv::Mat meas_set(measSize, 1, type);    // [z_x,z_y,z_w,z_h]
 
-        	}
-    	}
+					// curr_objects[assignment_to].objectState = state_set;
+					// curr_objects[assignment_to].objectMeas = meas_set;
+
+					// // insert new kalman filter here
+					// curr_objects[assignment_to].kalmanTracker = create_kalmanTracker();
+
+     //    	 	}else{	//found and update assignment
+
+     //    			curr_objects[assignment_to].objectMeas.at<float>(0) = curr_objects[assignment_to].topLeft.x + (curr_objects[assignment_to].bottomRight.x - curr_objects[assignment_to].topLeft.x)/2;;
+					// curr_objects[assignment_to].objectMeas.at<float>(1) = curr_objects[assignment_to].topLeft.y + (curr_objects[assignment_to].bottomRight.y - curr_objects[assignment_to].topLeft.y)/2;
+					// curr_objects[assignment_to].objectMeas.at<float>(2) = curr_objects[assignment_to].bottomRight.x - curr_objects[assignment_to].topLeft.x;
+					// curr_objects[assignment_to].objectMeas.at<float>(3) = curr_objects[assignment_to].bottomRight.y - curr_objects[assignment_to].topLeft.y;
+
+					// curr_objects[assignment_to].kalmanTracker = prev_objects[i].kalmanTracker;
+					// curr_objects[assignment_to].kalmanTracker.correct(curr_objects[assignment_to].objectMeas);		//correction
+     //    			curr_objects[assignment_to].id = prev_objects[i].id;
+     //    	 	}
+
+     //    	}
+    	// }
 
 
-	   	prev_objects.swap(curr_objects);
-	   	curr_objects.clear();
+    	// /******************************
+    	// 			Display 
+    	// ******************************/
+    	// cv::Mat display_img(640, 430, CV_8UC3, Scalar(0,0,0));	//dummy display
+
+    	// for(int k = 0; k < curr_objects.size(); k++){
+    	// 	// >>>> Matrix A
+     //  		curr_objects[k].kalmanTracker.transitionMatrix.at<float>(2) = dT;
+     //  		curr_objects[k].kalmanTracker.transitionMatrix.at<float>(9) = dT;
+     //  		// <<<< Matrix A
+
+     //  		curr_objects[k].objectState = curr_objects[k].kalmanTracker.predict();
+
+     //  		cv::Rect predRect;
+     //  		predRect.width = curr_objects[k].objectState.at<float>(4);
+     //  		predRect.height = curr_objects[k].objectState.at<float>(5);
+     //  		predRect.x = curr_objects[k].objectState.at<float>(0) - predRect.width / 2;
+     //  		predRect.y = curr_objects[k].objectState.at<float>(1) - predRect.height / 2;
+
+     //  		// label the box
+     //   		cv::Point2f namePos(predRect.tl().x,predRect.tl().y-10);
+     //   		cv::putText(display_img, "testing id", namePos, FONT_HERSHEY_PLAIN, 2.0, CV_RGB(0,128,0), 1.5);
+
+     //  		cv::rectangle(display_img, predRect, CV_RGB(0,128,0), 2);
+    	// }
+
+    	// cv::imshow("testing", display_img);
+    	// cv::waitKey(5);
+
+    	// // store the current objects and clear
+	   	 prev_objects.swap(curr_objects);
+	   	 curr_objects.clear();
 
    }
 }
